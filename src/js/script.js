6470c4b4233d3a4923021fb1919874f949458171
davidjaa <davidjaanson@gmail.com>
2023-11-05 14:27:30 +0200

    style and script update

--- src/js/script.js
+++ src/js/script.js
@@ -10,17 +10,32 @@ window.onload = function() {
             console.log(post.title, post.body)
             const postDiv = document.createElement('div')
             postDiv.className = 'image-box'
+
             const h3 = document.createElement('h3')
             const p = document.createElement('p')
-
+            const createTime = document.createElement('p')
+            const like = document.createElement('img')
             const img = document.createElement('img')
-            img.className = 'centered-image'
+            const profile = document.createElement('img')
+
+            img.className = 'centered-image border'
             h3.textContent = post.content
-            p.textContent= post.author.name
+            h3.className = 'comment-box-image'
+            p.textContent= '- ' + post.author.name
             img.src = post.image
+            createTime.textContent = post.createTime
+            createTime.className = 'date'
+            like.src = post.like
+            like.className = 'image-bottom-left'
+            profile.src = post.profile
+            profile.className = 'image-top-left'
+
+            postDiv.appendChild(img)
             postDiv.appendChild(h3)
             postDiv.appendChild(p)
-            postDiv.appendChild(img)
+            postDiv.appendChild(createTime)
+            postDiv.appendChild(like)
+            postDiv.appendChild(profile)
 
             const body = document.querySelector('section')
             body.appendChild(postDiv)
